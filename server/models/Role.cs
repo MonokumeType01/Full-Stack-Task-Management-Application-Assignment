39eb8a088afb533f9f66fab8209f9482a7ef960f
MonokumeType01 <wongshengxiang@gmail.com>
2025-05-26 01:53:54 +0800

    Update(backend): slight realign codes

--- server/models/Role.cs
+++ server/models/Role.cs
@@ -3,9 +3,9 @@ using System.ComponentModel.DataAnnotations;
 namespace TaskManagementApp.Models
 {
     public class Role
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
 
-}
+    }
 }
