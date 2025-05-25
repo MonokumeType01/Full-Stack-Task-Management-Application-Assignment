386850d3b13f6ed3e1f8e1d5252d188ae3c6667a
MonokumeType01 <wongshengxiang@gmail.com>
2025-05-26 01:02:04 +0800

    Update(frontend): Added new prop for Counter Component

--- client/src/components/CounterCard.js
+++ client/src/components/CounterCard.js
@@ -1,9 +1,9 @@
-export default function CounterCard({ num, title }) {
+export default function CounterCard({ title, num, maxNum }) {
 
   return (
     <div>
       <h2 className="text-xl font-semibold mb-2">{title}</h2>
-      <p className="text-4xl font-bold text-blue-600">{num}</p>
+      <p className="text-4xl font-bold text-blue-600">{num} {maxNum && <span> / {maxNum}</span>}</p> 
     </div>
   );
 }
