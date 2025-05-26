2fc08456336912c0b07cba16999507a6aeb31f62
MonokumeType01 <wongshengxiang@gmail.com>
2025-05-27 03:06:10 +0800

    Update(backend): Add user role to token

--- server/endpoints/auth.cs
+++ server/endpoints/auth.cs
@@ -25,26 +25,28 @@ public static class AuthEndpoints
         group.MapPost("/login", async ([FromBody] Models.User user, AppDbContext db) =>
         {
             var foundUser = await db.Users
+                .Include(u => u.Role)
                 .FirstOrDefaultAsync(u => u.Username == user.Username && u.Password == user.Password);
 
             if (foundUser is null)
                 return Results.Unauthorized();
 
 
-            var token = GenerateJwtToken(foundUser.Username, foundUser.Id.ToString(), config["Jwt:Issuer"]!, config["Jwt:Audience"]!, key);
+            var token = GenerateJwtToken(foundUser.Username, foundUser.Id.ToString(), foundUser.Role?.Name ?? "",config["Jwt:Issuer"]!, config["Jwt:Audience"]!, key);
             return Results.Ok(new { token });
         });
 
         return group;
     }
 
-    private static string GenerateJwtToken(string username, string userId, string issuer, string audience, SymmetricSecurityKey key)
+    private static string GenerateJwtToken(string username, string userId, string roleName, string issuer, string audience, SymmetricSecurityKey key)
     {
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
         var claims = new[]
         {
             new Claim(ClaimTypes.Name, username),
-            new Claim(ClaimTypes.NameIdentifier, userId)
+            new Claim(ClaimTypes.NameIdentifier, userId),
+            new Claim(ClaimTypes.Role, roleName)
         };
 
         var token = new JwtSecurityToken(
