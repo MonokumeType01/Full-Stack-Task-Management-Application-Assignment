ac2e76920c49a5b2cea9e075e203cfa3793fda72
MonokumeType01 <wongshengxiang@gmail.com>
2025-05-27 04:37:26 +0800

    Add(frontend): Protected Route Component to Redirect user back if not login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';       // adjust path if needed
 import Dashboard from './pages/Dashboard'; // make sure Dashboard component exists
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+              <Dashboard />
+          </ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   );
