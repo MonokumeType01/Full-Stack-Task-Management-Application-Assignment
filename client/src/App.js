6a1666ffd11c4a5c822b9ca7e94b14fc3351c319
MonokumeType01 <wongshengxiang@gmail.com>
2025-05-27 12:40:24 +0800

    Update(frontend): Redirect default path to login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';       // adjust path if needed
 import Dashboard from './pages/Dashboard'; // make sure Dashboard component exists
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -7,6 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace={true} />}/>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={
           <ProtectedRoute>
