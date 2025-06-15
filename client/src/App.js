5db34d1bd1de568c21fd0498a384e211371cd9ab
MonokumeType01 <wongshengxiang@gmail.com>
2025-06-15 19:40:34 +0800

    Update(frontend): Added toaster and signalr to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';       // adjust path if needed
 import Dashboard from './pages/Dashboard'; // make sure Dashboard component exists
 import ProtectedRoute from "./components/ProtectedRoute";
+import { startSignalRConnection, stopSignalRConnection } from "./signalr";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace={true} />}/>
         <Route path="/login" element={<Login />} />
