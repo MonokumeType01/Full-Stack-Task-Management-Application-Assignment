d04105b631ecf48a6790e62e5cf9ae4f31e0d875
MonokumeType01 <wongshengxiang@gmail.com>
2025-06-15 19:42:29 +0800

    Add(backend): Added user update DTO

---
+++ server/dto/updateUserDto.cs
@@ -0,0 +1,7 @@
+public class UpdateUserDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Username { get; set; }
+    public int? RoleId { get; set; }
+}
